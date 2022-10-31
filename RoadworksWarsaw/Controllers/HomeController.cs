7d31a1e171e72df2f017fd31dbef5dea4bd3479a
Paula Wojciechowska <paulla.wojciechowska@gmail.com>
2022-10-31 18:05:29 +0100

    Add error handling

--- RoadworksWarsaw/Controllers/HomeController.cs
+++ RoadworksWarsaw/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using RoadworksWarsaw.Models;
 using RoadworksWarsaw.Models.Config;
 using RoadworksWarsaw.Models.RoadworksDetails;
 using RoadworksWarsaw.Models.RoadworksInfo;
-using System.Diagnostics;
 
 namespace RoadworksWarsaw.Controllers
 {
@@ -20,36 +19,8 @@ namespace RoadworksWarsaw.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var client = new RestClient("https://api.um.warszawa.pl/api");
-            var request = new RestRequest("action/get_open_invests");
-            request.AddParameter("resource_id", "26b9ade1-f5d4-439e-84b4-9af37ab7ebf1");
-            request.AddParameter("pageSize", 100);
-            request.AddParameter("startIndex", 1);
-            request.AddParameter("apikey", APIKey.ApiKey);
-            var response = await client.GetAsync<OpenInvests>(request);
-            return View(response);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetRoadworksDetails(string investId)
-        {
-            var client = new RestClient("https://api.um.warszawa.pl/api");
-            var request = new RestRequest("action/get_open_invest_details");
-            request.AddParameter("resource_id", "25feb40c-f26a-428b-89ba-27ffefa795a5");
-            request.AddParameter("investId", $"{investId}");
-            request.AddParameter("apikey", APIKey.ApiKey);
-            var response = await client.GetAsync<OpenInvestsDetails>(request);
-            return View(response);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Search(string street)
-        {
-            if (string.IsNullOrEmpty(street))
-            {
-                return RedirectToAction("Index");
-            }
-            else
+            _logger.LogInformation("action=index");
+            try
             {
                 var client = new RestClient("https://api.um.warszawa.pl/api");
                 var request = new RestRequest("action/get_open_invests");
@@ -57,23 +28,67 @@ namespace RoadworksWarsaw.Controllers
                 request.AddParameter("pageSize", 100);
                 request.AddParameter("startIndex", 1);
                 request.AddParameter("apikey", APIKey.ApiKey);
-                request.AddParameter("streetName", $"{street}");
                 var response = await client.GetAsync<OpenInvests>(request);
-
-                ViewBag.SearchString = street;
-                return View("Index", response);
+                return View(response);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"action=index, msg='{ex.Message}'", ex);
+                return View("Error", new ErrorViewModel());
             }
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public async Task<IActionResult> GetRoadworksDetails(string investId)
         {
-            return View();
+            _logger.LogInformation($"action=GetRoadworksDetails, investId={investId}");
+            try
+            {
+                var client = new RestClient("https://api.um.warszawa.pl/api");
+                var request = new RestRequest("action/get_open_invest_details");
+                request.AddParameter("resource_id", "25feb40c-f26a-428b-89ba-27ffefa795a5");
+                request.AddParameter("investId", $"{investId}");
+                request.AddParameter("apikey", APIKey.ApiKey);
+                var response = await client.GetAsync<OpenInvestsDetails>(request);
+                return View(response);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"action=GetRoadworksDetails, msg='{ex.Message}'", ex);
+                return View("Error", new ErrorViewModel());
+            }
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public async Task<IActionResult> Search(string street)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            _logger.LogInformation($"action=search, street={street}");
+            try
+            {
+                if (string.IsNullOrEmpty(street))
+                {
+                    _logger.LogWarning("action=search, msg='Street not found succesfully'");
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var client = new RestClient("https://api.um.warszawa.pl/api");
+                    var request = new RestRequest("action/get_open_invests");
+                    request.AddParameter("resource_id", "26b9ade1-f5d4-439e-84b4-9af37ab7ebf1");
+                    request.AddParameter("pageSize", 100);
+                    request.AddParameter("startIndex", 1);
+                    request.AddParameter("apikey", APIKey.ApiKey);
+                    request.AddParameter("streetName", $"{street}");
+                    var response = await client.GetAsync<OpenInvests>(request);
+                    ViewBag.SearchString = street;
+                    return View("Index", response);
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"action=search, msg='{ex.Message}'", ex);
+                return View("Error", new ErrorViewModel());
+            }
         }
     }
 }
--- RoadworksWarsaw/Models/ErrorViewModel.cs
+++ RoadworksWarsaw/Models/ErrorViewModel.cs
@@ -2,8 +2,14 @@ namespace RoadworksWarsaw.Models
 {
     public class ErrorViewModel
     {
+        public ErrorViewModel()
+        {
+            this.Message = "Something went wrong, please try again later.";
+        }
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public object Message { get; private set; }
     }
 }
